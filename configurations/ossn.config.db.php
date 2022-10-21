d9280213878d912d1ecca631ad6e5bb4a7c0c5b1
Arafatmollik1 <arafathossain923@gmail.com>
2022-10-22 01:43:59 +0300

    more commits

---
+++ configurations/ossn.config.db.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Open Source Social Network
+ *
+ * @package   (openteknik.com).ossn
+ * @author    OSSN Core Team <info@openteknik.com>
+ * @copyright (C) OpenTeknik LLC
+ * @license   Open Source Social Network License (OSSN LICENSE)  http://www.opensource-socialnetwork.org/licence
+ * @link      https://www.opensource-socialnetwork.org/
+ */
+
+
+// replace localhost with your database host name;
+$Ossn->host = 'localhost';
+
+// replace 3306 with your database host name;
+$Ossn->port = '3306';
+
+// replace u626900164_test_user with your database username;
+$Ossn->user = 'u626900164_test_user';
+
+// replace Pussycat1 with your database password;
+$Ossn->password = 'Pussycat1';
+
+// replace u626900164_test_ossn with your database name;
+$Ossn->database = 'u626900164_test_ossn';
